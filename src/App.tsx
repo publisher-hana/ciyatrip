0ce6426af23ff78d2b42aa9b2291b36b82404656
unknown <lhn1208@naver.com>
2024-12-30 03:15:55 +0900

    라우터 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,28 @@
 import Tour from "./pages/Tour";
 import { Home, Layout, TripDetails } from "./utils/Route";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Route,  HashRouter as Router, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
-    <Routes>
-      <Route path ="/" element={
-        <Layout>
-          <Home />
-        </Layout>
-      } />
-      <Route path ="/datalist/:id" element={
-        <Layout>
-          <TripDetails />
-        </Layout>
-      } />
-      <Route path ="/tour" element={
-        <Layout>
-          <Tour />
-        </Layout>
-      } />
-    </Routes>
-  </BrowserRouter>
+    <Router>
+      <Routes>
+        <Route path ="/" element={
+          <Layout>
+            <Home />
+          </Layout>
+        } />
+        <Route path ="/datalist/:id" element={
+          <Layout>
+            <TripDetails />
+          </Layout>
+        } />
+        <Route path ="/tour" element={
+          <Layout>
+            <Tour />
+          </Layout>
+        } />
+      </Routes>
+  </Router>
   );
 }
 
