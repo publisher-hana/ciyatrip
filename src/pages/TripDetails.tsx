72b09472c9fbe0bea67a3ef484fbb481c2ebf040
unknown <lhn1208@naver.com>
2024-12-30 03:58:48 +0900

    수정

--- src/pages/TripDetails.tsx
+++ src/pages/TripDetails.tsx
@@ -46,9 +46,9 @@ export const TripDetails = () => {
       <section className="mt-20">
         <div className="img h-[50vh] relative">
           <div className="absolute top-0 left-0 w-full h-[50vh] bg-[rgba(16,38,54,.6)]"></div>
-          <img src="../images/back.jpg" alt="" className="w-full h-full object-cover" />
+          <img src="/ciyatrip/images/back.jpg" alt="" className="w-full h-full object-cover" />
           <div className="absolute lef-0 bottom-0 w-full h-10">
-            <img src="../images/back2.png" alt="" className="w-full h-full object-cover" />
+            <img src="/ciyatrip/images/back2.png" alt="" className="w-full h-full object-cover" />
           </div>
         </div>
         <div className="containers">
